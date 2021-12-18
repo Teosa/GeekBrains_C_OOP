aed99833edc336b5459594d9559c1e25b2e474aa
Teosa Landers <lama_07@mail.ru>
2021-12-18 22:13:46 +0300

    Урок 6. Потоки ввода-вывода

--- Lesson06/Lesson06.cpp
+++ Lesson06/Lesson06.cpp
@@ -1,6 +1,70 @@
 
+#include <iostream>
+#include <regex>
+using namespace std;
+
+/* Задание 1
+* Создать программу, которая считывает целое число типа int.
+* И поставить «защиту от дурака»: если пользователь вводит что-то кроме одного целочисленного значения,
+* нужно вывести сообщение об ошибке и предложить ввести число еще раз. Пример неправильных введенных строк:
+    rbtrb
+    nj34njkn
+    1n
+ */
+void task01()
+{
+    bool correctInput = false;
+    string value;
+    const auto regexp = regex("^\\d{1,}$");
+    
+    while(!correctInput)
+    {
+        cout << "Enter value: ";
+        cin >> value;
+
+        if(regex_match(value, regexp))
+        {
+            cout << "Input " << value << " is correct." << endl;
+            correctInput = true;
+        } else
+        {
+            cout << "ERROR: Input " << value << " is incorrect." << endl;
+        } 
+    }
+}
+
+/* Задание 2
+ * Создать собственный манипулятор endll для стандартного потока вывода, который выводит два перевода строки и сбрасывает буфер.
+ */
+ostream& custom_endl(ostream &stream) { 
+    stream << endl << endl;
+    return stream;
+}
+
+void task02()
+{
+    cout << "Test" << custom_endl;
+    cout << "Test2" << endl;
+    cout << "Test3" << custom_endl;
+    cout << "Test4" << endl;
+
+    /* Output:
+    * Test
+    *
+    * Test2
+    * Test3
+    * 
+    * Test4
+    *
+    * Process finished with exit code 0.
+    */
+}
+
+
+
 int main()
 {
-    
+    task01();
+    task02();
     return 0;
 }
