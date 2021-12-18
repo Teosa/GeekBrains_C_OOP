ba7e4330cae3732dde34d5095de4200cee4e0b3f
Teosa Landers <lama_07@mail.ru>
2021-12-18 23:56:47 +0300

    Урок 7. Поддержка модульности. Написание игры Blackjack

---
+++ Lesson07/Date.cpp
@@ -0,0 +1,47 @@
+﻿#include "Date.h"
+
+#include <iostream>
+
+template<class T>
+int compare(T first, T second)
+{
+    if(first > second) return 1;
+    if(first < second) return -1;
+    return 0;
+}
+
+Date::Date() : day(1), month(1), year(1990) { }
+Date::Date(int const day, int const month, int const year) : day(day), month(month), year(year) { }
+
+void Date::setDay(int const day) { this->day = day; }
+void Date::setMonth(int const month) { this->month = month; }
+void Date::setYear(int const year) { this->year = year; }
+
+int Date::getDay() { return day; };
+int Date::getMonth() { return month; };
+int Date::getYear() { return year; };
+
+bool Date::operator<(Date& anotherDate)
+{
+    if(compare(this->getYear(), anotherDate.getYear()) == -1) return true;
+    if(compare(this->getMonth(), anotherDate.getMonth()) == -1) return true;
+    if(compare(this->getDay(), anotherDate.getDay()) == -1) return true;
+
+    return false;
+}
+
+bool Date::operator>(Date& anotherDate)
+{
+    if(compare(this->getYear(), anotherDate.getYear()) == 1) return true;
+    if(compare(this->getMonth(), anotherDate.getMonth()) == 1) return true;
+    if(compare(this->getDay(), anotherDate.getDay()) == 1) return true;
+
+    return false;
+}
+
+ostream& operator<<(ostream& stream, Date& date)
+{
+    return stream << date.getDay() << "-" << date.getMonth() << "-" << date.getYear();
+}
+
+
---
+++ Lesson07/Date.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <ostream>
+
+using namespace std;
+
+class Date
+{
+    int day;
+    int month;
+    int year;
+public:
+    Date();
+    Date(int const day, int const month, int const year);
+    
+    void setDay(int const day);
+    void setMonth(int const month);
+    void setYear(int const year);
+
+    int getDay();
+    int getMonth();
+    int getYear();
+
+    bool operator>(Date& anotherDate);
+    bool operator<(Date& anotherDate);
+
+   friend ostream& operator<<(ostream& stream,  Date& date);
+};
